3da550f8b80d69d67e4caef64e38f8fc7d379d21
Talita Azevedo <talita.azevedo360@gmail.com>
2020-04-11 21:21:44 -0300

    Handle Like Repository

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect}from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,103 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+
+  
+  const [ repositories, setRepositories] = useState([]);
+  useEffect(()=>{
+    api.get('repositories').then(response => {
+
+      setRepositories(response.data);
+    });
+    
+
+  },[]);
+  
+
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    
+    repositories[repositoryIndex].likes =  (await api.post(`repositories/${id}/like`)).data.likes;
+
+    
+    const newRepo = [...repositories];
+    setRepositories(newRepo);
+    
+    
+    
+    
+
+    
+
+    
   }
+
+
+
+  
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+        <FlatList
+          
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({item:repository})=>(
+            <View
+              style={styles.repositoryContainer}
             >
-              3 curtidas
-            </Text>
-          </View>
+              <Text style={styles.repository}> {repository.title}
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              </Text>{/* title */}
+
+               <View style={styles.techsContainer}>
+                
+                  {repository.techs
+                    .toString()
+                    .split(',')
+                    .map(
+                      (tech, index) => 
+                        <Text  key={`${tech[index]}-${index}-${repository.id}`}style={styles.tech}>
+                          {tech}              
+                        </Text>
+                    )}
+                
+                
+
+              </View>{/* techsContainer  */}
+              <View style={styles.likesContainer}>
+                  <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repository.id}`}
+                  >
+                  {`${repository.likes} curtidas`}
+                </Text>
+              </View>{/* likes Container */}
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(repository.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${repository.id}`}
+              >
+              <Text style={styles.buttonText}>Curtir</Text>
+
+             </TouchableOpacity>{/* Like Button */}
+            </View>
+
+
+          )}
+        />
+       
       </SafeAreaView>
     </>
   );
@@ -95,6 +152,8 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    
+    
   },
   buttonText: {
     fontSize: 14,
@@ -103,5 +162,6 @@ const styles = StyleSheet.create({
     color: "#fff",
     backgroundColor: "#7159c1",
     padding: 15,
+    
   },
 });
